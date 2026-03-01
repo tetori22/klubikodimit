0b155ed0b40069d08b7a2188970cf09a72f025fb
tetori22 <tetoriuser0@gmail.com>
2026-03-01 15:30:33 +0100

    Add files via upload

---
+++ projekt.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import random
+
+st.set_page_config(page_title="Gëzuar 7-8 Marsin 🌸")
+
+st.title("🌷 Kartolinë Digjitale për 7–8 Marsin")
+
+opsion = st.radio("Zgjidhni festën:", ["7 Mars - Dita e Mësuesit", 
+                                       "8 Mars - Dita e Nënës"])
+
+emri = st.text_input("Shkruani emrin:")
+
+if st.button("Shfaq Urimin 💌"):
+    
+    if opsion.startswith("7"):
+        urime = [
+            "Ju jeni drita që ndriçon rrugën tonë drejt dijes.",
+            "Faleminderit për përkushtimin dhe zemrën tuaj të madhe.",
+            "Çdo mësim nga ju është një hap drejt suksesit tonë."
+        ]
+        titulli = "🌟 GËZUAR 7 MARSIN! 🌟"
+    else:
+        urime = [
+            "Dashuria dhe forca juaj na frymëzon çdo ditë.",
+            "Ju jeni zemra e familjes dhe burim gëzimi.",
+            "Mirënjohje për gjithçka që bëni me kaq përkushtim."
+        ]
+        titulli = "💖 GËZUAR 8 MARSIN! 💖"
+
+    st.markdown(f"## {titulli}")
+    st.success(f"{emri}, {random.choice(urime)}")
+    st.balloons()
